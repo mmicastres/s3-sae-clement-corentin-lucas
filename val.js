af591f8da422357f6d537172603b2ea87413a5e4
Corentin Rivieres <cocorivieres@gmail.com>
2023-12-11 15:42:08 +0100

    val.js_opendata

--- val.js
+++ val.js
@@ -1,1 +1,7 @@
-
+fetch('agent.json')
+    .then(response => response.json())
+    .then(data => {
+     
+      console.log(data);
+    })
+    .catch(error => console.error('Erreur lors de la récupération du fichier JSON:', error));
