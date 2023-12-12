bcb5bed956565819789e29e8827d71282e356cd3
CLorieau <clement.lorieau@gmail.com>
2023-12-12 15:29:04 +0100

    v1.03

--- val.js
+++ val.js
@@ -1,7 +1,19 @@
-fetch('agent.json')
-    .then(response => response.json())
-    .then(data => {
-     
-      console.log(data);
+
+let fetchOptions = { method: 'GET' } // les options de fetch
+// executer la req AJAX
+fetch('agent.json', fetchOptions)
+    .then((response) => { return response.json() })
+    .then((dataJSON) => {
+        console.log(dataJSON)
+      let agents = dataJSON.data
+                let texteHTML = ""
+
+                for (let agent of agents){
+                    texteHTML += `<li><a class="dropdown-item" href="agent.html/#${agent.displayName}">${agent.displayName}</a></li>`
+                    
+                }
+                document.getElementById("agentmenu").innerHTML = texteHTML;
+
     })
+  
     .catch(error => console.error('Erreur lors de la récupération du fichier JSON:', error));
