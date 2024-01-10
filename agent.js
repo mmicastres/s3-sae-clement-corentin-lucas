9ad77a531f8fb4b3fc5e9d93186e92290b53c436
Corentin Rivieres <cocorivieres@gmail.com>
2024-01-10 17:46:34 +0100

    add background agent et opti list agent

--- agent.js
+++ agent.js
@@ -14,14 +14,16 @@ fetch(url, fetchOptions)
             // Utiliser une classe pour inverser l'ordre des éléments "left" et "right" alternativement
             const orderClass = i % 2 === 0 ? 'order-left' : 'order-right';
 
+            //A faire : ajouter une 3eme div centrale pour y mettre la description et abilitie afin qu'elle ne bouge pas et reste au centre
+
             txt += `<section class="d-flex ${orderClass}" id="${agent.displayName}">
-            <div class="bg-primary text-white col-4" id="left">
+            <div class="bg-primary text-white col-3" id="left">
             <h3 class="d-flex justify-content-center"><strong>${agent.displayName}</strong></h3>
             <img class="img-fluid" id="portrait" src="${agent.fullPortrait}"> </div> 
-            <div class="col-8 d-flex align-items-center" id="right">
+            <div class="col-9 d-flex align-items-center" id="right">
                 <div class="d-flex flex-column">
-                <p class="text-start mb-4"> ${agent.description}</p>
-                <p class="mb-4"><strong> ${agent.role.displayName}</strong></p>
+                <p id="description" class="text-start mb-4 condensed-description"> ${agent.description}</p>
+                <p id="role_agent" class="mb-4"><strong> ${agent.role.displayName}</strong></p>
                 <div id="abilitie" class="d-flex flex-row">
                     <img class="img-fluid filter mx-4" id="abilitie" src="${agent.abilities[0].displayIcon}">
                     <img class="img-fluid filter mx-4" id="abilitie" src="${agent.abilities[1].displayIcon}">
@@ -42,16 +44,15 @@ fetch(url, fetchOptions)
     .then((response) => { return response.json() })
     .then((dataJSON) => {
         console.log(dataJSON)
-      let agents = dataJSON.data
-                let texteHTML = ""
+        let agents = dataJSON.data
+        let texteHTML = ""
 
-                for (let agent of agents){
-                    texteHTML += `<li><a class="dropdown-item" href="#${agent.displayName}">${agent.displayName}</a></li>`
-                    
-                }
-                document.getElementById("agentmenu").innerHTML = texteHTML;
+        for (let agent of agents) {
+            texteHTML += `<li><a class="dropdown-item" href="#${agent.displayName}">${agent.displayName}</a></li>`
+
+        }
+        document.getElementById("agentmenu").innerHTML = texteHTML;
 
     })
-  
-    .catch(error => console.error('Erreur lors de la récupération du fichier JSON:', error));
 
+    .catch(error => console.error('Erreur lors de la récupération du fichier JSON:', error));
