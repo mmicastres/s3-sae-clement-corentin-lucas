6a163d5cd5d3411b67a1e407977035a4d21c73eb
Corentin Rivieres <cocorivieres@gmail.com>
2024-01-10 15:31:58 +0100

    agent js inverse left right

--- agent.js
+++ agent.js
@@ -1,30 +1,43 @@
-const url = "agent.json" // l’url de la ressource de l’API
-let fetchOptions = { method: 'GET' } // les options de fetch
+const url = "agent.json"; // l’URL de la ressource de l’API
+let fetchOptions = { method: 'GET' }; // les options de fetch
+
 // executer la req AJAX
 fetch(url, fetchOptions)
     .then((response) => { return response.json() })
     .then((dataJSON) => { // dataJSON = les données renvoyées
         console.log(dataJSON)
         let results = dataJSON.data;
-        let txt = ''
-        for (agent of results) {
+        let txt = '';
+        for (let i = 0; i < results.length; i++) {
+            const agent = results[i];
+            const isEven = i % 2 === 0;
+
+            // Inverser l'ordre des éléments "left" et "right" alternativement
+            const leftOrRight = isEven ? 'left' : 'right';
+
             txt += `<section class="d-flex flex-row" id="${agent.displayName}">
-            <div class="bg-primary text-white col-4" id="left">
+            <div class="bg-primary text-white col-4" id="${leftOrRight}">
             <h3 class="d-flex justify-content-center"><strong>${agent.displayName}</strong></h3>
-            <img class="img-fluid" id="portrait" src="${agent.fullPortrait
-                }"> </div> <div class="col-8 d-flex align-items-center" id="right">
+            <img class="img-fluid" id="portrait" src="${agent.fullPortrait}"> </div> 
+            <div class="col-8 d-flex align-items-center" id="${isEven ? 'right' : 'left'}">
                 <div class="d-flex flex-column">
-                <p class="text-start mb-3"> ${agent.description}</p>
-                <p class="mb-3"><strong> ${agent.role.displayName}</strong></p>
-                <div id="abilitie" class="d-flex flex-row"><img class="img-fluid filter mx-4" id="abilitie" src="${agent.abilities[0].displayIcon}"><img class="img-fluid filter mx-4" id="abilitie" src="${agent.abilities[1].displayIcon}"><img class="img-fluid filter mx-4" id="abilitie" src="${agent.abilities[2].displayIcon}"><img class="img-fluid filter mx-4" id="abilitie" src="${agent.abilities[3].displayIcon}"></div>
-                <div></div></section>`
-            //Afficher agents
-            document.getElementById('agents').innerHTML = txt
+                <p class="text-start mb-4"> ${agent.description}</p>
+                <p class="mb-4"><strong> ${agent.role.displayName}</strong></p>
+                <div id="abilitie" class="d-flex flex-row">
+                    <img class="img-fluid filter mx-4" id="abilitie" src="${agent.abilities[0].displayIcon}">
+                    <img class="img-fluid filter mx-4" id="abilitie" src="${agent.abilities[1].displayIcon}">
+                    <img class="img-fluid filter mx-4" id="abilitie" src="${agent.abilities[2].displayIcon}">
+                    <img class="img-fluid filter mx-4" id="abilitie" src="${agent.abilities[3].displayIcon}">
+                </div>
+                <div></div></section>`;
+
+            // Afficher agents
+            document.getElementById('agents').innerHTML = txt;
         }
     })
     .catch((error) => {
-        console.log(error) // gestion des erreurs
-    })
+        console.log(error); // gestion des erreurs
+    });
 
 // executer la req AJAX
 fetch(url, fetchOptions)
