afb70b5a4c796a47dae00b80c4dc99af17ae8d3c
Lucas Boustens <lucasboustens@gmail.com>
2024-02-02 10:25:37 +0100

    site visualisation version finale

--- maps.js
+++ maps.js
@@ -1,47 +1,86 @@
-const url = "maps.json"; // fichier json
+const url = "maps.json";
 const url2 = "agent.json"; // fichier json
 let fetchOptions = { method: 'GET' }; // les options de fetch
 
+document.addEventListener('DOMContentLoaded', () => {
+    // Attendez que le DOM soit complètement chargé avant d'ajouter des écouteurs d'événements.
+    // Ajoutez des écouteurs d'événements à tous les boutons avec la catégorie correspondante
+    document.getElementById('Haven').addEventListener('click', () => loadMaps('Haven'));
+    document.getElementById('Icebox').addEventListener('click', () => loadMaps('Icebox'));
+    document.getElementById('Pearl').addEventListener('click', () => loadMaps('Pearl'));
+    document.getElementById('Sunset').addEventListener('click', () => loadMaps('Sunset'));
+    document.getElementById('Lotus').addEventListener('click', () => loadMaps('Lotus'));
+    document.getElementById('Breeze').addEventListener('click', () => loadMaps('Breeze'));
+    document.getElementById('Bind').addEventListener('click', () => loadMaps('Bind'));
+    document.getElementById('Fracture').addEventListener('click', () => loadMaps('Fracture'));
+    document.getElementById('Split').addEventListener('click', () => loadMaps('Split'));
+    document.getElementById('Ascent').addEventListener('click', () => loadMaps('Ascent'));
+});
 
-// Charger le fichier maps.json
-fetch(url, fetchOptions)
-  .then(response => response.json())
-  .then(dataJSON => {
-    // Traitement des données de maps.json
-    console.log('Données de maps.json:', dataJSON);
 
-    // Charger le fichier agent.json
-    return fetch(url2, fetchOptions);
-  })
-  .then((response) => { return response.json(); })
-  .then((dataJSON) => {
-    // Traitement des données de agent.json
-    console.log('Données de agent.json:', dataJSON);
 
-    let results = dataJSON.data;
-        let txt = '';
+async function loadAgent() {
+    try {
+        const response = await fetch(url2);
+        const dataJSON = await response.json();
 
-        for (map of results) {
-            txt += `<div class="${map.saison.join(' ')}">
-                <h2 class="text-center py-3">${map.displayName}</h3>
-                <img class="img-fluid" src="${map.splash}">
-            </div>`;
-        }
-    document.getElementById('maps').innerHTML = txt;
+        // Utilisation de la méthode map pour extraire displayName et displayIcon
+        const agentsInfo = dataJSON.data.map(agent => ({
+            displayName: agent.displayName,
+            displayIcon: agent.displayIcon
+        }));
 
-    const button9 = document.getElementById('all');
-    const button8 = document.getElementById('s8');
-    const button7 = document.getElementById('s7');
-    const button6 = document.getElementById('s6');
-    const button5 = document.getElementById('s5');
-    const button4 = document.getElementById('s4');
-    const button3 = document.getElementById('s3');
-    const button2 = document.getElementById('s2');
-    const button = document.getElementById('s1');
-  })
-  .catch(error => {
-    console.error('Erreur lors du chargement des fichiers JSON', error);
-  });
+        return agentsInfo;
+    } catch (error) {
+        console.error('Error loading the agent data:', error);
+    }
+}
+
+
+function loadMaps(displayName) {
+    fetch(url, fetchOptions)
+        .then(response => response.json())
+        .then(dataJSON => {
+            // Récupérer les cartes avec le displayName spécifié
+            let maps = dataJSON.data.filter(map => map.displayName === displayName);
+
+            // Résoudre la promesse loadAgent
+            loadAgent().then(agentsInfo => {
+                console.log(agentsInfo);
+                let txt = '';
+
+                maps.forEach(map => {
+                    // Utilisez une boucle pour itérer sur les saisons si c'est un tableau
+                    txt += `<div class="${map.Agents.join(' ')} col-md-2">
+                    <h2 class="text-center py-3">${map.displayName}</h2>
+                    <img class="img-fluid" src="${map.splash}">
+                </div>`;
+
+                    // Vérifie si les noms des agents existent dans le tableau Agents du fichier maps.json
+                    map.Agents.forEach(agentDisplayName => {
+                        const matchingAgent = agentsInfo.find(agent => agent.displayName === agentDisplayName);
+                        if (matchingAgent) {
+                            txt += `<div class="col-md-2 d-flex flex-column justify-content-center align-items-center">
+                                        <h4 class="text-center">${agentDisplayName}</h4>
+                                        <img class="img-fluid w-50 text-center" src="${matchingAgent.displayIcon}">
+                                    </div>`;
+                        }
+                    });
+
+
+                });
+
+                document.getElementById('agentmaps').innerHTML = txt;
+            });
+
+        })
+        .catch(error => {
+            console.error('Error loading the maps data:', error);
+        });
+}
+
+
+
 
 
 fetch(url, fetchOptions)
@@ -53,10 +92,11 @@ fetch(url, fetchOptions)
         for (map of results) {
             txt += `
             <div class="${map.saison.join(' ')} col-md-4">
-                <h2 class="text-center py-3">${map.displayName}</h3>
+                <h2 id="${map.displayName}" class="text-center py-3">${map.displayName}</h2>
                 <img class="img-fluid" src="${map.splash}">
-            </div>`;}
-        
+            </div>`;
+        }
+
 
         document.getElementById('maps').innerHTML = txt;
 
@@ -197,5 +237,4 @@ ouvrir.addEventListener('click', function (e) {
 window.onclick = function () {
     filter.classList.remove("filters--active");
 };
-
 
